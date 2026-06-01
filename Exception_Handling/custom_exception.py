719b0921461ddf53ec584d68f3910e8afc90ec16
Aryan <aryansaini8773@gmail.com>
2026-06-01 08:26:30 +0530

    Custom Exception

--- Exception_Handling/custom_exception.py
+++ Exception_Handling/custom_exception.py
@@ -1,6 +1,10 @@
+class Myerror(Exception):
+    def __init__(self, num):
+        super().__init__(f"{num} is less than 0. Must be +ve.")
+
 a = int(input("Enter a number: "))
 if a<0:
-    raise ValueError("I don't divide negative numbers.")
+    raise Myerror(a)
 try:
     print(10/int(a))
 except Exception as e:

1b0434184eafb33a2f2c0bb797241e0cce00182a
Aryan <aryansaini8773@gmail.com>
2026-06-08 17:32:47 +0530

    Learned Fast-API

---
+++ REST-API/stackOverflow_api.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+response = requests.get("http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow")
+print(response)  ##<Response [200]>
+for data in response.json()['items']:
+    if data['answer_count']==0:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print('skipped')
+    print()
